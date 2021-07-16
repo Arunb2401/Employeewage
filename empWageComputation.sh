6433c4bbe3b7e018907ea1ba0559818785007c86
Arunb401 <arunbiradar24@gmail.com>
2021-07-16 19:01:14 +0530

    Modified : Calculating Half Time wage feature added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,21 @@
 #!/bin/bash -x
 
 #constants
-IS_PRESENT=1
-empcheck=$((RANDOM%2))
+IS_PRESENT_HALF_TIME=1
 EMP_RATE_PER_HR=20
 
-if [ $empcheck -eq $IS_PRESENT ]
-then
- empHr=8
-else
- empHr=0
-fi
+#variable
+empcheck=$(( RANDOM % 2 ))
 
-salary=$(( $empHr * $EMP_RATE_PER_HR ))
+#Selection
+if [ $empcheck -eq 1 ]
+then
+ 	empHr=8
+	salary=$(( empHrs * EMP_RATE_PER_HR ))
+#elif [ $empcheck -eq 2 ]
+#then
+#	empHr=16
+#	salary=$(( empHrs * EMP_RATE_PR_HR ))
+#else
+#	salary=0
+fi
